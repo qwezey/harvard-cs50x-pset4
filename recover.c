2de3f91352a28d249bf380f3cb2fa309b9e80d42
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2020-06-02 06:55:26 -0400

    get file instead

--- recover.c
+++ recover.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/// Returns the filename from given command line arguments.
-/// If bad arguments are passed, print error and return NULL.
-char* parseFilename(int argc, char** argv) {
+/// Returns the file specified by the command line arguments.
+/// If bad arguments are passed or issue opening file, print
+/// error and return NULL. The returned file must be closed.
+FILE* getFile(int argc, char** argv) {
     if (argc > 2) {
         printf("This program only accepts one argument\n");
         return NULL;
     }
-    return argv[1];
+    char* filename = argv[1];
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        printf("There was an issue reading the file: %s\n");
+        return NULL;
+    }
+    return file;
 }
 
 int main(int argc, char** argv) {
-    char* filename = parseFilename(argc, argv);
-    if (!filename) return 1;
+    FILE* file = getFile(argc, argv);
+    if (!file) return 1;
+    fclose(file);
     return 0;
 }
