2c8bf4316c331550b7df9dbb487abf72eb4c9580
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2020-06-02 06:40:29 -0400

    parse args

--- recover.c
+++ recover.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
+/// Returns the filename from given command line arguments.
+/// If bad arguments are passed, print error and return 0.
+char* parseFilename(int argc, char** argv) {
+    if (argc > 2) {
+        printf("This program only accepts one argument");
+        return 0;
+    }
+    return argv[1];
+}
 
+int main(int argc, char** argv) {
+    char* filename = parseFilename(argc, argv);
+    if (!filename) return 1;
+    return 0;
 }
