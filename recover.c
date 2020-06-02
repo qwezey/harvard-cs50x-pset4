f0cc76bc55c70313ff4ddc7dffbde7d00e495376
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2020-06-02 07:47:36 -0400

    raw jpeg file

--- recover.c
+++ recover.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/// A "lost" raw JPEG file defined in this assignment.
+/// [bytes] is the bytes of the file, and [size] is the size.
+typedef struct {
+    char* bytes;
+    long size;
+} RawJpegFile;
+
+/// Returns a raw JPEG file at path [filename]. Returns NULL on error.
+/// Must be closed with [closeRawJpegFile()].
+RawJpegFile* openRawJpegFile(char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (!file) return NULL;
+    if (fseek(file, 0, SEEK_END)) return NULL;
+    long size = ftell(file);
+    if (size == -1) return NULL;
+    char* bytes = malloc(size);
+    rewind(file);
+    if (!fread(bytes, size, 1, file)) return NULL;
+    if (fclose(file)) return NULL;
+    RawJpegFile* ret = malloc(sizeof(RawJpegFile));
+    ret->bytes = bytes;
+    ret->size = size;
+    return ret;
+}
+
+/// Closes resources of [file] returned by [openRawJpegFile()].
+void closeRawJpegFile(RawJpegFile* file) {
+    free(file->bytes);
+    free(file);
+}
+
 /// Returns the file specified by the command line arguments.
 /// If bad arguments are passed or issue opening file, print
 /// error and return NULL. The returned file must be closed.
-FILE* getFile(int argc, char** argv) {
+RawJpegFile* getFile(int argc, char** argv) {
     if (argc > 2) {
         printf("This program only accepts one argument\n");
         return NULL;
     }
     char* filename = argv[1];
-    FILE* file = fopen(filename, "r");
+    RawJpegFile* file = openRawJpegFile(filename);
     if (!file) {
         printf("There was an issue reading the file: %s\n", filename);
         return NULL;
@@ -19,8 +50,8 @@ FILE* getFile(int argc, char** argv) {
 }
 
 int main(int argc, char** argv) {
-    FILE* file = getFile(argc, argv);
+    RawJpegFile* file = getFile(argc, argv);
     if (!file) return 1;
-    fclose(file);
+    closeRawJpegFile(file);
     return 0;
 }
